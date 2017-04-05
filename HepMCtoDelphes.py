dd96b635d4a199c049fbe8423a911add14c7c9e9
guzelgun <guzelgun@users.noreply.github.com>
2017-04-05 16:44:18 +0300

    Add files via upload

--- HepMCtoDelphes.py
+++ HepMCtoDelphes.py
@@ -1,11 +1,17 @@
 import subprocess
 from subprocess import call
 import os
+import sys
 
 #os.chdir directory degistirme adina verildi.
 #subprocess bulunulan directoryde calisiyor, "cd"yi algilamiyor.
-os.chdir('programs/Delphes-3.4.0/')
+os.chdir('programs/Delphes-3.3.0/')
+
+outputFileName = raw_input("Please indicate output file name and path as path/name.root: ")
+
+inputFileName = raw_input("Please indicate input file name and path as path/name.hepmc: ")
 
 #Delphes komutu. root dosyasinin adini ve hepmc'nin adini/pathini degistirin
-subprocess.call(["./DelphesHepMC", "cards/delphes_card_CMS.tcl", "PY_W_Drell_10K.root", "../pythia8223/examples/PY_W_Drell_10K.hepmc"])
+subprocess.call(["./DelphesHepMC", "cards/delphes_card_CMS.tcl", outputFileName, inputFileName])
+
 
