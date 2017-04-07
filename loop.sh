4fbf5511d0da237376215a15fb57c4a9e455cbed
guzelgun <guzelgun@users.noreply.github.com>
2017-04-07 10:52:11 +0300

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Path for hepmc files
+# Gather all hepmc files in a separate directory. Root output will also be saved in there.
 FILE=/Users/ceren/programs/pythia8223/examples/*.hepmc
 
 for f in $FILE
@@ -8,5 +9,7 @@ for f in $FILE
 	do
 		echo "Processing $f file..."
 		cd /Users/ceren/programs/Delphes-3.3.0/	
+# "${f%.hepmc}.root" names the output root file same as the hepmc.
+# ex: TTbar.hepmc > TTbar.root
 		./DelphesHepMC cards/delphes_card_CMS.tcl "${f%.hepmc}.root" $f
 done
